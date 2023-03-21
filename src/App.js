06c7c207eac65f09da97aef17a6bc64f0971fc8f
VarnerDamascenoJr <varnerdjr@gmail.com>
2023-03-20 21:33:50 -0300

    Validation in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import { Register } from './components/Register';
+import Register from './components/Register';
+
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Register/>
     </div>
   );
 }
